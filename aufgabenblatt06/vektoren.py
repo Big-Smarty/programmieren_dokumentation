435f8373c5b3709d8bb904a6b9e8d7affd22b5e5
Big Smarty <77147643+Big-Smarty@users.noreply.github.com>
2024-11-04 09:31:59 +0100

    Add files via upload

---
+++ aufgabenblatt06/vektoren.py
@@ -0,0 +1,21 @@
+def vektor_summe(a, b):
+    """Addiere die Vektoren elementweise (a0, a1, a2) und (b0, b1, b2) werden zu (a0+b0, a1+b1, a2+b2)"""
+    out = []
+    for i in range(len(a)):
+        out.append(a[i] + b[i])
+    return out
+
+
+def skalar_produkt(a, b):
+    """Multipliziere die Vektoren elementweise (a0, a1, a2) und (b0, b1, b2) werden zu (a0*b0, a1*b1, a2*b2)"""
+    out = []
+    for i in range(len(a)):
+        out.append(a[i] * b[i])
+    return out
+
+
+def skaliere(k, a):
+    """Multipliziere alle Elemente in a mit k sodass (a0, a1, a2) und k zu (a0*k, a1*k, a2*k) werden"""
+    for i in range(len(a)):
+        a[i] *= k
+    return a
